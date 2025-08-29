b480d3a1e79e04da01372d52c2a674390c1c870a
waldo1234567 <waldowalerian@gmail.com>
2025-08-29 15:00:45 +0700

    fixing deployment issue pt 6

--- src/main/java/com/latihan/latihan/Config/CorsConfig.java
+++ src/main/java/com/latihan/latihan/Config/CorsConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -14,7 +15,13 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration cfg = new CorsConfiguration();
-        cfg.setAllowedOrigins(List.of("https://*.vercel.app"));
+        cfg.setAllowedOriginPatterns(Arrays.asList(
+                "https://*.vercel.app",
+                "https://aura-front-8yzq269p1-waldo1234567s-projects.vercel.app",
+                "https://aura-front-wine.vercel.app/",
+                "http://localhost:3000",
+                "http://127.0.0.1:3000"
+        ));
         cfg.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         cfg.setAllowedHeaders(List.of("*"));
         cfg.setAllowCredentials(true);
